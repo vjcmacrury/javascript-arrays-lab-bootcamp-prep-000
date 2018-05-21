b869a0cb9a146af51cc826a502e90fb7c3b07db9
vjcmacrury@gmail.com MacRury <vjcmacrury@gmail.com>
2018-05-21 15:06:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
+
 function destructivelyAppendKitten(kitty1) {
   kittens.push("Ralph");
+  return(kittens)
+}
+
+function destructivelyPrependKitten(kitty2) {
+  kittens.unshift("Ralph");
   return(kittens)
 }
